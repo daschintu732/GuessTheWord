df1865022537fb7b6a7d1fece60e520ca97fa809
daschintu732 <daschintu732@gmail.com>
2024-04-17 12:47:24 +0530

    modified all

--- src/main/java/com/guess_the_word/controller/GameController.java
+++ src/main/java/com/guess_the_word/controller/GameController.java
@@ -4,22 +4,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.guess_the_word.service.GameService;
+import com.guess_the_word.utils.GameUtils;
 
 @Controller
 public class GameController {
 
 	@Autowired
-	GameService service;
+	private GameService service;
+
+	@Autowired
+	private GameUtils utils;
 
 	@GetMapping("/homepage")
-	public String showHomePage(Model model) {
+	public String showHomePage(@RequestParam(value = "guessedChar", required = false) String guessedChar, Model model) {
 
 		String randomWord = service.toString();
 
-		model.addAttribute("wordToDisplay", randomWord);
+		if (guessedChar != null) {
+			
+			boolean isGuessCorrect = service.addGuess(guessedChar.charAt(0));
 
+			randomWord = service.toString();
+
+			if (isGuessCorrect == false) {
+				utils.reduceTry();
+			}
+		}
+
+		//System.out.println("Number of Tries Remaining : "+utils.getTriesRemaining());
+		
+		model.addAttribute("wordToDisplay", randomWord);
+		
+		model.addAttribute("triesLeft",utils.getTriesRemaining());
+		
 		return "home-page";
 	}
 }
--- src/main/java/com/guess_the_word/service/GameService.java
+++ src/main/java/com/guess_the_word/service/GameService.java
@@ -7,15 +7,52 @@ import org.springframework.stereotype.Service;
 @Service
 public class GameService {
 
+	private String randomlyChoosenWord = null;
+
 	private String[] words = { "father", "mother", "hello", "world", "java", "mysql", "python", "oracle" };
 
+	private char[] allCharactersOfTheword;
+
 	Random random = new Random();
-	
+
+	public GameService() {
+
+		randomlyChoosenWord = words[random.nextInt(words.length)];
+		System.out.println("Random Word is : " + randomlyChoosenWord);
+		allCharactersOfTheword = new char[randomlyChoosenWord.length()];
+	}
+
+	@Override
 	public String toString() {
-	
-		//System.out.println(words.length);
+
+		String ret = "";
+
+		for (char c : allCharactersOfTheword) {
+			if (c == '\u0000') {
+				ret = ret + "_";
+			}
+			else {
+				ret = ret + c;
+			}
+			
+			ret = ret + ' ';
+		}
+		return ret;
+
+	}
+
+	public boolean addGuess(char guessedChar) {
+
+		boolean isGuessCorrect = false; 
 		
-		return words[random.nextInt(words.length)];
+		for (int i = 0; i < randomlyChoosenWord.length(); i++) {
 		
+			if(guessedChar == randomlyChoosenWord.charAt(i)) {
+				
+				allCharactersOfTheword[i] = guessedChar;
+				isGuessCorrect = true;
+			}
+		}
+		return isGuessCorrect;
 	}
 }
